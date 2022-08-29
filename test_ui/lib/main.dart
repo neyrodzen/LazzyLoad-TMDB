4025d3841b4850df72788f59c0d0d40588300d47
Aleksandr Kononov <neyrodzen@yandex.ru>
2022-08-29 13:15:55 +0500

    create project for experimets

---
+++ test_ui/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+            child: Container(
+          color: Colors.blue,
+        )),
+      ),
+    );
+  }
+}
