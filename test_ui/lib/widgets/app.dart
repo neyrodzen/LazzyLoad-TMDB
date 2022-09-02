0204bf7f02fb9edcc53706afac82df97b157cbb0
Aleksandr Kononov <neyrodzen@yandex.ru>
2022-09-02 14:05:32 +0500

    ListView

--- test_ui/lib/widgets/app.dart
+++ test_ui/lib/widgets/app.dart
@@ -10,32 +10,13 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  final controller = ScrollController(initialScrollOffset: 2000.0);
-
-  @override
-  void initState() {
-    super.initState();
-    controller.addListener(() {
-      print('$controller.offset');
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final list = <Widget>[];
-    for (int i = 0; i < 200; i++) {
-      list.add(Text('$i'));
-    }
-    return SingleChildScrollView(
-      controller: controller,
-      // ignore: sized_box_for_whitespace
-      child: Container(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: list,
-        ),
-      ),
-    );
+    return ListView.builder(itemBuilder: (BuildContext context, int index) {
+      return Container(
+        color: (index % 2 == 0 ? Colors.amber : Colors.blue),
+        padding: const EdgeInsets.all(100),
+      );
+    });
   }
 }
